94b605f1e568ec04bdf5efc7cc9f0562312f0a0e
Ayush Bajracharya <bajracharya.ayush@webexpertsnepal.com>
2021-04-20 17:18:03 +0545

    from our blogs section on home page updated with custom query

--- archive.php
+++ archive.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -11,41 +16,40 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+  <div class="page-banner">
+				<div class="page-banner__bg-image" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ocean.jpg);"></div>
+				<div class="page-banner__content container container--narrow">
+				<h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+				<div class="page-banner__intro">
+					<p><?php the_archive_description(); ?></p>
+				</div>
+				</div>  
+			</div>
+	
+			<div class="container container--narrow page-section">
+      <?php
+        while(have_posts()){
+          the_post(); ?>
+          <div class="post-item">
+            <h2 class = "headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="metabox">
+              <p>posted by <?php the_author_posts_link(); ?> on <?php the_time('s M Y - T g:i:s a'); ?> on <?php echo get_the_category_list(', '); ?></p>
+            </div>
+            <div class="generic-content">
+              <?php the_excerpt(); ?>
+              <p><a href="<?php the_permalink(); ?>" class="btn btn--blue">Continue reading &raquo;</a></p>
+            </div>
+          </div>
+        <?php }
+        echo paginate_links();
+      ?>
+    
+			</div>
+	  
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
+
